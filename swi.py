ef850601ae6cba5cce7d907488b2cb92b96641c0
Terry Stewart <tcstewar@uwaterloo.ca>
2015-01-07 21:08:56 -0500

    Added websocket example

--- swi.py
+++ swi.py
@@ -514,6 +514,33 @@ if __name__ == '__main__':
             <p>Click <a href="page">here</a> to the first page.
             <p>Click <a href="otherPage">here</a> to go to another page.
             '''
+
+        # http://localhost:8080/timer
+        def swi_timer(self):
+            return """<html><head><script>
+    function doInit() {
+        var s;
+        try {
+            s = new WebSocket("ws://localhost:8080/timer");
+            s.onopen = function(e) { console.log("connected");};
+            s.onclose = function(e) { console.log("connection closed");};
+            s.onerror = function(e) { console.log("connection error");};
+            s.onmessage = function(e) { console.log("message: " + e.data);};
+        } catch(ex) {
+            console.log("connection exception: " + ex);
+        }
+    }
+    </script></head>
+    <body onload="doInit();">
+    </body>
+    </html>"""
+
+        def ws_timer(self, client):
+            import time
+            while True:
+                client.write(time.strftime('%H:%M:%S'))
+                time.sleep(1)
+
     Demo.add_user('terry', 'password')
     browser(8080)
     start(Demo, 8080)
